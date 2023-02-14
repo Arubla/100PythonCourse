ef61c5162ddf9d6ffe57b84c12950b3f284b7c45
arubla <arunblatchley@gmail.com>
2023-02-13 21:11:18 -0500

    input

--- Day1PrintingStart/Input.py
+++ Day1PrintingStart/Input.py
@@ -1,3 +1,8 @@
 name = input("What is your name?")
 
 print("Nice to meet you " + name)
+
+#console will prompt for the input using input params
+#then print the concatenation
+print("Hello, " + 
+input("what is your friend's name?"))
