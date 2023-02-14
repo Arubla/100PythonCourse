278f9ccb0373e5b419d12e347182a967a868620c
arubla <arunblatchley@gmail.com>
2023-02-13 21:38:36 -0500

    band name generator

---
+++ Day1PrintingStart/BandNameGenerator.py
@@ -0,0 +1,5 @@
+welcome = "Welcome to the Band Name Generator"
+
+print(welcome)
+
+print("Your band name could be " + input("What's the name of the city you grew up in?\n") + " " + input("What was your first pet?\n"))
